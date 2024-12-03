af38a2018334cbd6b46c8adb673258ba9c00a709
Sven Reinck <sreinck@fluxparticle.de>
2024-12-03 08:08:11 +0100

    Aufgabe: Echtzeitkommunikation mit WebSockets

--- src/main/java/com/example/chat/ChatPushBean.java
+++ src/main/java/com/example/chat/ChatPushBean.java
@@ -1,6 +1,8 @@
 package com.example.chat;
 
 import jakarta.enterprise.context.*;
+import jakarta.faces.push.*;
+import jakarta.inject.*;
 import org.slf4j.*;
 
 @ApplicationScoped
@@ -8,9 +10,14 @@ public class ChatPushBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChatPushBean.class);
 
+    @Inject
+    @Push
+    private PushContext chatChannel;
+
     public void onChatMessage(ChatMessageEvent event) {
         String message = event.getMessage();
         LOGGER.info("Nachricht an Benutzer gesendet: {}", message);
+        chatChannel.send(message);
     }
 
 }
